92d9ebfa556e3a189cc12461fb7c40ff1abad8aa
safwan <mohammedsafwan261205@gmail.com>
2025-08-18 13:55:04 +0530

    swapped sqlalchemy for sqlite3

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sqlite3, os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+BASE_DIR = os.path.abspath(os.path.dirname("todo.db"))
 
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}"
+def dbConnection():
+    connection = sqlite3.connect(os.path.join(BASE_DIR, 'todo.db'))
+    return connection
+
+# class Todo(db.Model):
+#     sno = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(200), nullable=False)
+#     desc = db.Column(db.String(500), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self) -> str:
+#         return f"{self.sno} - {self.title}"
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title=title, desc=desc)
-        db.session.add(todo)
-        db.session.commit()
+        # todo = Todo(title=title, desc=desc)
+        # db.session.add(todo)
+        # db.session.commit()
+
+        db = dbConnection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO todo (title, description) VALUES (?,?)", (title, desc))
+        db.commit()
         
-    allTodo = Todo.query.all() 
+    # allTodo = Todo.query.all() 
+    db = dbConnection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM TODO;")
+    allTodo = cursor.fetchall()
+    print(allTodo)
     return render_template('index.html', allTodo=allTodo)
 
 @app.route('/show')
